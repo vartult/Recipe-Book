d21301c5de572beb3251e698ff40c1ac60d9d3eb
Vartul Tripathi <vartult2@gmail.com>
2019-06-15 02:49:21 +0530

    Add: Retrofit2 Calling

--- app/src/main/java/com/cellfishpool/requests/Recipe_Api.java
+++ app/src/main/java/com/cellfishpool/requests/Recipe_Api.java
@@ -1,4 +1,28 @@
 package com.cellfishpool.requests;
 
+import com.cellfishpool.requests.responses.RecipeResponse;
+import com.cellfishpool.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface Recipe_Api {
+    public interface RecipeApi {
+
+        // SEARCH
+        @GET("api/search")
+        Call<RecipeSearchResponse> searchRecipe(
+                @Query("key") String key,
+                @Query("q") String query,
+                @Query("page") String page
+        );
+
+        // GET RECIPE REQUEST
+        @GET("api/get")
+        Call<RecipeResponse> getRecipe(
+                @Query("key") String key,
+                @Query("rId") String recipe_id
+        );
+    }
 }
