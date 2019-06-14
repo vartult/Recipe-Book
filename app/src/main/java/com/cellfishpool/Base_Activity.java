addc888e48d9e9c692f1727d1f23b7b93188e1a6
Vartul Tripathi <vartult2@gmail.com>
2019-06-14 20:23:18 +0530

    Enhancement: Reusability

--- app/src/main/java/com/cellfishpool/Base_Activity.java
+++ app/src/main/java/com/cellfishpool/Base_Activity.java
@@ -1,5 +1,27 @@
 package com.cellfishpool;
 
-public class Base_Activity {
-    
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+public abstract class Base_Activity extends AppCompatActivity {
+
+    ProgressBar mProgressBar=null;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        ConstraintLayout constraintLayout= (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
+        FrameLayout mFrameLayout = constraintLayout.findViewById(R.id.activity_container);
+        mProgressBar=constraintLayout.findViewById(R.id.progress);
+
+        getLayoutInflater().inflate(layoutResID, mFrameLayout, true);
+
+        super.setContentView(constraintLayout);
+    }
+
+    public void showProgressBar(boolean visibility){
+        mProgressBar.setVisibility(visibility ? View.VISIBLE : View.INVISIBLE);
+    }
 }
