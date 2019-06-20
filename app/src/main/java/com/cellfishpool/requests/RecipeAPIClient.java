81aa0d0e720af18a5b694d6281408df46e92d997
Vartul Tripathi <vartult2@gmail.com>
2019-06-21 02:37:15 +0530

    MVVM: Retrofit Query

--- app/src/main/java/com/cellfishpool/requests/RecipeAPIClient.java
+++ app/src/main/java/com/cellfishpool/requests/RecipeAPIClient.java
@@ -2,19 +2,27 @@ package com.cellfishpool.requests;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
 
 import com.cellfishpool.models.Recipe;
 import com.cellfishpool.AppExecutors;
+import com.cellfishpool.requests.responses.RecipeSearchResponse;
 import com.cellfishpool.util.Constants;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class RecipeAPIClient {
 
     private static RecipeAPIClient instance;
     private MutableLiveData<List<Recipe>> mRecipes;
+    private RetrieveRecipesRunnable mretrieveRecipesRunnable;
 
     public static RecipeAPIClient getInstance(){
         if(instance == null){
@@ -31,14 +39,12 @@ public class RecipeAPIClient {
         return mRecipes;
     }
 
-    public void searchRecipeAPI(){
-        final Future handler = AppExecutors.getInstance().NetworkIO().submit(new Runnable() {
-            @Override
-            public void run() {
-                //retrive data
-                //
-            }
-        });
+    public void searchRecipeAPI(String query, int pagegNumber){
+        if(mretrieveRecipesRunnable!=null){
+            mretrieveRecipesRunnable=null;
+
+        }
+        final Future handler = AppExecutors.getInstance().NetworkIO().submit(mretrieveRecipesRunnable);
 
         AppExecutors.getInstance().NetworkIO().schedule(new Runnable() {
             @Override
@@ -47,5 +53,59 @@ public class RecipeAPIClient {
                 handler.cancel(true);
             }
         }, Constants.NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
+    private class RetrieveRecipesRunnable implements Runnable{
+        private String query;
+        private int pageNumber;
+        boolean cancel_req;
+
+        public RetrieveRecipesRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancel_req=false;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Response response=getRecipes(query,pageNumber).execute();
+                if(cancel_req==true){
+                    return;
+                }
+                if(response.code()==200){
+                    List<Recipe> list = new ArrayList<>(((RecipeSearchResponse)response.body()).getRecipes());
+                    if(pageNumber==1){
+                        mRecipes.postValue(list);
+                    }
+                    else {
+                        List<Recipe> currentRecipe = mRecipes.getValue();
+                        currentRecipe.addAll(list);
+
+                        mRecipes.postValue(currentRecipe);
+                    }
+                }
+                else{
+                    String error = response.errorBody().string();
+                    Log.e("Hello", "run: "+error);
+                    mRecipes.postValue(null);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                mRecipes.postValue(null);
+            }
+        }
+
+        private Call<RecipeSearchResponse> getRecipes(String query,int pageNumber){
+            return ServiceGenerator.getRecipeApi().searchRecipe(
+                    Constants.API_Key,
+                    query,
+                    String.valueOf(pageNumber)
+            );
+        }
+
+        private void cancelRequest(){
+            Log.d("hello","Cancel Request: Cancelling the search query");
+        }
     }
 }
