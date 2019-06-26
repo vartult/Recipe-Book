791e0772ef929621c540751025facb7c011073e5
Vartul Tripathi <vartult2@gmail.com>
2019-06-27 03:23:41 +0530

    Recipe Main Activity

---
+++ app/src/main/java/com/cellfishpool/Recipe_Activity.java
@@ -0,0 +1,54 @@
+package com.cellfishpool;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import com.cellfishpool.models.Recipe;
+import android.support.v7.widget.AppCompatImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+
+public class Recipe_Activity extends Base_Activity {
+
+    // UI components
+    private AppCompatImageView mRecipeImage;
+    private TextView mRecipeTitle, mRecipeRank;
+    private LinearLayout mRecipeIngredientsContainer;
+    private ScrollView mScrollView;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_recipe);
+        mRecipeImage = findViewById(R.id.recipe_image);
+        mRecipeTitle = findViewById(R.id.recipe_title);
+        mRecipeRank = findViewById(R.id.recipe_social_score);
+        mRecipeIngredientsContainer = findViewById(R.id.ingredients_container);
+        mScrollView = findViewById(R.id.parent);
+
+        getComingIntent();
+    }
+
+
+    private void getComingIntent(){
+        if(getIntent().hasExtra("recipe")){
+            Recipe recipe = getIntent().getParcelableExtra("recipe");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
